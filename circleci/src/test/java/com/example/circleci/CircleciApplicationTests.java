b3f93c9b42d7433c3112e7db81873b5faa6b6844
hoshi-R <hs.rt7771@gmail.com>
2022-04-04 14:52:07 +0900

    4

--- circleci/src/test/java/com/example/circleci/CircleciApplicationTests.java
+++ circleci/src/test/java/com/example/circleci/CircleciApplicationTests.java
@@ -1,6 +1,15 @@
 package com.example.circleci;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +17,25 @@ class CircleciApplicationTests {
 
 	@Test
 	void contextLoads() {
+		try {
+			System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			
+			driver.get("https://www.google.com/");
+      File sfile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+      Date date = new Date();
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyy'/'MM'/'dd'/'E'/'k'/'mm'/'ss'/'");
+      try {
+        Files.copy(sfile.toPath(), new File(sdf.format(date) + ".png").toPath());
+      } catch (Exception e) {
+      }
+      System.out.println(sfile);
+      Thread.sleep(2000);
+      driver.quit();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
